ae6c306dab2702472e0151817ba243982218c3ff
Pavel Kasyanov <pavel.kasyanov@gmail.com>
2022-01-25 10:58:14 +0300

    Updated the readme file, added 2 brand new code snippets

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Various Web of Science APIs use cases
 
 * [Auhtor-level Fractional Counting for Organizations](/author-level_fractional_counting_for_organizations/)
-* [Various Types of Self-citation](/various_types_of_self_citation/)
+* [Simple H-Index without Self-citations code](/researcherid-based_h-index_excluding_self-citations/)
+* [Comprehensive Various Types of Self-citation code](/various_types_of_self_citation/)
+* [Cities Collaboration analysis](/cities_collaboration_analysis)
--- various_types_of_self_citation/main.py
+++ various_types_of_self_citation/main.py
@@ -1,43 +1,34 @@
 import requests
+from paper import Paper
 from apikey import apikey   # Your API key, it's better not to store it in the program
 
 # Enter the WoS search query to evaluate its self-citation percentage:
-search_query = '(TS=("self citation*" or selfcitation*)) AND (TP==("HIGHLY CITED PAPERS"))'
+search_query = 'AI=A-3080-2009'
 
 headers = {
     'X-APIKey': apikey
 }
 
-endpoint = "https://api.clarivate.com/api/wos"
+baseurl = "https://api.clarivate.com/api/wos"
 
-# This will save several API queries/records received by storing the already checked citing papers locally
+# This will save several API queries/records by storing the already checked citing papers locally
 checked_citing_papers = [('ut', 'cited_paper')]
 
 
-class CitedPaper:
-    def __init__(self, ut, author_names, author_dais, author_rids, author_orcids, org_names, source_names, times_cited,
-                 citing_papers_list):
-        self.ut = ut
-        self.author_names = author_names
-        self.author_dais = author_dais
-        self.author_rids = author_rids
-        self.author_orcids = author_orcids
-        self.org_names = org_names
-        self.source_names = source_names
+class CitedPaper(Paper):
+    def __init__(self, ut, author_names, author_dais, author_rids, author_orcids, org_names, country_names,
+                 source_name, times_cited, citing_papers_list):
+        super().__init__(ut, author_names, author_dais, author_rids, author_orcids, org_names, country_names,
+                 source_name)
         self.times_cited = times_cited
         self.citing_papers_list = citing_papers_list
 
 
-class CitingPaper:
-    def __init__(self, ut, author_names, author_dais, author_rids, author_orcids, org_names, source_names,
-                 self_citation_crs):
-        self.ut = ut
-        self.author_names = author_names
-        self.author_dais = author_dais
-        self.author_rids = author_rids
-        self.author_orcids = author_orcids
-        self.org_names = org_names
-        self.source_names = source_names
+class CitingPaper(Paper):
+    def __init__(self, ut, author_names, author_dais, author_rids, author_orcids, org_names, country_names,
+                 source_name, self_citation_crs):
+        super().__init__(ut, author_names, author_dais, author_rids, author_orcids, org_names, country_names,
+                 source_name)
         self.self_citation_crs = self_citation_crs
 
 
@@ -48,22 +39,23 @@ def cited_papers():  # This is how we create a list of cited papers based on a s
         ut = paper['UID']
         author_names, author_dais, author_rids, author_orcids = get_author_list(paper)
         org_names = get_org_list(paper)
-        source_names = get_source_list(paper)
+        country_names = get_country_list(paper)
+        source_name = get_source_name(paper)
         times_cited = get_times_cited(paper)
         citing_papers_list = []
         cited_papers_list.append(CitedPaper(ut, author_names, author_dais, author_rids, author_orcids, org_names,
-                                            source_names, times_cited, citing_papers_list))
+                                            country_names, source_name, times_cited, citing_papers_list))
     return cited_papers_list
 
 
 def cited_request():  # This function actually gets the cited paper data via API
     cited_data = []
-    initial_response = requests.get(f'{endpoint}?databaseId=WOS&usrQuery={search_query}&count=0&firstRecord=1',
+    initial_response = requests.get(f'{baseurl}?databaseId=WOS&usrQuery={search_query}&count=0&firstRecord=1',
                                     headers=headers)
     initial_data = initial_response.json()
     for i in range(((initial_data['QueryResult']['RecordsFound'] - 1) // 100) + 1):
         subsequent_response = requests.get(
-            f'{endpoint}?databaseId=WOS&usrQuery={search_query}&count=100&firstRecord={(100 * i + 1)}',
+            f'{baseurl}?databaseId=WOS&usrQuery={search_query}&count=100&firstRecord={(100 * i + 1)}',
             headers=headers)
         print(f"Getting cited papers data: {i+1} of {((initial_data['QueryResult']['RecordsFound'] - 1) // 100) + 1}")
         addtl_data = subsequent_response.json()
@@ -81,10 +73,11 @@ def citing_papers(cited_papers_list):  # Based on the list of cited papers, we g
             ut = record['UID']
             author_names, author_dais, author_rids, author_orcids = get_author_list(record)
             org_names = get_org_list(record)
-            source_names = get_source_list(record)
+            country_names = get_country_list(record)
+            source_name = get_source_name(record)
             self_citation_crs = 0
             paper.citing_papers_list.append(CitingPaper(ut, author_names, author_dais, author_rids, author_orcids,
-                                                        org_names, source_names, self_citation_crs))
+                                                        org_names, country_names, source_name, self_citation_crs))
             #  Please pay attention to the line above: every object of CitingPaper class is an item of
             #  citing_papers_list, an attribute of specific cited paper. By this, we establish links between cited and
             #  citing records. This allows finding self-citations only in the records that reference each other, not
@@ -95,11 +88,11 @@ def citing_papers(cited_papers_list):  # Based on the list of cited papers, we g
 
 def citing_request(paper):  # This function  gets the citing paper data via API
     citing_data = []
-    initial_response = requests.get(f'{endpoint}/citing?databaseId=WOS&uniqueId={paper.ut}&count=0&firstRecord=1',
+    initial_response = requests.get(f'{baseurl}/citing?databaseId=WOS&uniqueId={paper.ut}&count=0&firstRecord=1',
                                     headers=headers)
     initial_data = initial_response.json()
     for i in range(((initial_data['QueryResult']['RecordsFound'] - 1) // 100) + 1):
-        subsequent_response = requests.get(f'{endpoint}/citing?databaseId=WOS&uniqueId={paper.ut}&count=100&firstRecord={(100*i+1)}', headers=headers)
+        subsequent_response = requests.get(f'{baseurl}/citing?databaseId=WOS&uniqueId={paper.ut}&count=100&firstRecord={(100*i+1)}', headers=headers)
         addtl_data = subsequent_response.json()
         for j in range(len(addtl_data['Data']['Records']['records']['REC'])):
             citing_data.append(addtl_data['Data']['Records']['records']['REC'][j])
@@ -113,6 +106,27 @@ def get_author_list(paper):  # This function gets lists of authors (and coauthor
     author_orcids = set()  # This set relies on author ORCID
     if paper['static_data']['summary']['names']['count'] == 1:
         author_names.add(paper['static_data']['summary']['names']['name']['wos_standard'])
+        author_dais.add(paper['static_data']['summary']['names']['name']['daisng_id'])
+        try:
+            for rid in paper['static_data']['summary']['names']['name']['data-item-ids']['data-item-id']:
+                if rid['id-type'] == 'PreferredRID':
+                    author_rids.add(rid['content'])
+        except KeyError:
+            pass  # No RID data in this author record
+        except TypeError:
+            # A rare case when the RID is linked to the author, but the record isn't claimed
+            try:
+                if paper['static_data']['contributors']['count'] == 1:
+                    author_rids.add(paper['static_data']['contributors']['contributor']['name']['r_id'])
+                else:
+                    for contributor in paper['static_data']['contributors']['contributor']:
+                        author_rids.add(contributor['name']['r_id'])
+            except KeyError:
+                pass # Okay, there's just no ResearcherID data in the paper
+        try:
+            author_orcids.add(paper['static_data']['summary']['names']['name']['orcid_id'])
+        except KeyError:
+            pass  # No ORCID data in this author record
     else:
         for person_name in paper['static_data']['summary']['names']['name']:
             try:
@@ -130,7 +144,15 @@ def get_author_list(paper):  # This function gets lists of authors (and coauthor
             except KeyError:
                 pass  # No RID data in this author record
             except TypeError:
-                pass  # A rare case when the RID is linked to the author, but the record isn't claimed
+                # A rare case when the RID is linked to the author, but the record isn't claimed
+                try:
+                    if paper['static_data']['contributors']['count'] == 1:
+                        author_rids.add(paper['static_data']['contributors']['contributor']['name']['r_id'])
+                    else:
+                        for contributor in paper['static_data']['contributors']['contributor']:
+                            author_rids.add(contributor['name']['r_id'])
+                except KeyError:
+                    pass  # Okay, there's just no ResearcherID data in the paper
             try:
                 author_orcids.add(person_name['orcid_id'])
             except KeyError:
@@ -155,12 +177,25 @@ def get_org_list(paper):  # This function gets lists of affiliated organizations
     return org_names
 
 
-def get_source_list(paper):  # This function gets lists of publication sources for every paper
-    source_list = set()  # The set relies on Abbreviated Source Name field of every record
+def get_country_list(paper):
+    country_names = set()
+    try:
+        if paper['static_data']['fullrecord_metadata']['addresses']['count'] == 1:
+            country_names.add(paper['static_data']['fullrecord_metadata']['addresses']['address_name']['address_spec']['country'])
+        else:
+            for affiliation in paper['static_data']['fullrecord_metadata']['addresses']['address_name']:
+                country_names.add(affiliation['address_spec']['country'])
+    except KeyError:
+        pass  # When there is no address data on the paper record at all
+    return country_names
+
+
+def get_source_name(paper):  # This function gets publication sources for every paper
+    source_name = ""  # The string relies on Abbreviated Source Name field of every record
     for title in paper['static_data']['summary']['titles']['title']:
         if title['type'] == 'source_abbrev':
-            source_list.add(title['content'])
-    return source_list
+            source_name = title['content']
+    return source_name
 
 
 def get_times_cited(paper):  # This function gets the times cited count for every cited paper
@@ -180,11 +215,11 @@ def self_citation_crs_calc(cited_paper, citing_paper):
         if checked_citing_paper[0] == citing_paper.ut:
             cr_data = checked_citing_paper[1]
     else:  # If it hasn't - the code will send a request to Web of Science API for cited references of that paper
-        initial_response = requests.get(f'{endpoint}/references?databaseId=WOS&uniqueId={citing_paper.ut}&count=100&firstRecord=1', headers=headers)
+        initial_response = requests.get(f'{baseurl}/references?databaseId=WOS&uniqueId={citing_paper.ut}&count=100&firstRecord=1', headers=headers)
         cr_data = initial_response.json()
         for i in range(((cr_data['QueryResult']['RecordsFound'] - 1) // 100)):
             subsequent_response = requests.get(
-                f'{endpoint}/references?databaseId=WOS&uniqueId={citing_paper.ut}&count=100&firstRecord={(100 * (i + 1) + 1)}',
+                f'{baseurl}/references?databaseId=WOS&uniqueId={citing_paper.ut}&count=100&firstRecord={(100 * (i + 1) + 1)}',
                 headers=headers)
             addtl_cr_data = subsequent_response.json()
             for paper in range(len(addtl_cr_data['Data'])):
@@ -203,6 +238,7 @@ def self_citations(cited_papers_list):  # Self-citation calculations occur here
     author_rids_self_citation = 0
     author_orcids_self_citation = 0
     org_self_citation = 0
+    country_self_citation = 0
     source_self_citation = 0
     for cited_paper in cited_papers_list:  # For every cited paper we run a check
         for citing_paper in cited_paper.citing_papers_list:  # Every paper that was citing it is checked for matches
@@ -226,7 +262,11 @@ def self_citations(cited_papers_list):  # Self-citation calculations occur here
                 if citing_paper.self_citation_crs == 0:
                     self_citation_crs_calc(cited_paper, citing_paper)
                 org_self_citation += citing_paper.self_citation_crs
-            if len(cited_paper.source_names.intersection(citing_paper.source_names)) > 0:  # For the titles those papers were published in
+            if len(cited_paper.country_names.intersection(citing_paper.country_names)) > 0:  #For their country names
+                if citing_paper.self_citation_crs == 0:
+                    self_citation_crs_calc(cited_paper, citing_paper)
+                country_self_citation += citing_paper.self_citation_crs
+            if cited_paper.source_name == citing_paper.source_name:  # For the titles those papers were published in
                 if citing_paper.self_citation_crs == 0:
                     self_citation_crs_calc(cited_paper, citing_paper)
                 source_self_citation += citing_paper.self_citation_crs
@@ -235,8 +275,9 @@ def self_citations(cited_papers_list):  # Self-citation calculations occur here
     print(f'    DAIS-level: {(author_dais_self_citation/total_citations * 100):.2f}% ({author_dais_self_citation} self-citations, {total_citations - author_dais_self_citation} external, {total_citations} total)')
     print(f'    ResearcherID-level: {(author_rids_self_citation/total_citations * 100):.2f}% ({author_rids_self_citation} self-citations, {total_citations - author_rids_self_citation} external, {total_citations} total)')
     print(f'    ORCID-level: {(author_orcids_self_citation/total_citations * 100):.2f}% ({author_orcids_self_citation} self-citations, {total_citations - author_orcids_self_citation} external, {total_citations} total)')
-    print(f'Organization-level self citation: {(org_self_citation/total_citations * 100):.2f}% ({org_self_citation} self-citations, {total_citations - org_self_citation} external, {total_citations} total)')
-    print(f'Publication Source-level self citation: {(source_self_citation/total_citations * 100):.2f}% ({source_self_citation} self-citations, {total_citations - source_self_citation} external, {total_citations} total)')
+    print(f'Organization-level self-citation: {(org_self_citation/total_citations * 100):.2f}% ({org_self_citation} self-citations, {total_citations - org_self_citation} external, {total_citations} total)')
+    print(f'Country-level self-citation: {(country_self_citation/total_citations * 100):.2f}% ({country_self_citation} self-citations, {total_citations - country_self_citation} external, {total_citations} total)')
+    print(f'Publication Source-level self-citation: {(source_self_citation/total_citations * 100):.2f}% ({source_self_citation} self-citations, {total_citations - source_self_citation} external, {total_citations} total)')
 
 
 a = cited_papers()
