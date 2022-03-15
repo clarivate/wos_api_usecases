5fb672a6b7573d7d127e1b55949d43f403c2992c
LTPB\U8017762 <pavel.kasyanov@clarivate.com>
2022-03-15 19:30:47 +0400

    Simplified the code by removing the our_org autofill feature which was confusing, impoved the results representation.

--- cities_collaborations_analysis/Readme.md
+++ cities_collaborations_analysis/Readme.md
@@ -1,7 +1,7 @@
 # Cities Collaborations Analysis
 
 
-## A simple script to evaluate a set of Web of Science docuents for the cities in which this research has been made - or to evaluate a specific organization for which cities it mostly collaborates with. The code relies on Web of Science article data obtained using Web of Sciencce Expanded API
+## A simple script to evaluate a set of Web of Science docuents for the cities in which this research has been made - or to evaluate a specific organization for which cities it mostly collaborates with. The code relies on Web of Science article data obtained using Web of Sciencce Expanded API.
 
 This is how the code works
 
@@ -13,5 +13,83 @@ This is how the code works
 And launch the code.
 
 The program will query Web of Science Expanded API for the document data, extract the "City" fields from each of them, process this data and print out the top 10 cities associated this set of papers (or collaborating with the organizaition provided). The full list of cities will be saved into a .csv-file in the same project folder.
+
+Two main use cases of this code are:
+1. Checking which cities is research on a specific topic concentrated in.
+
+Example: we want to figure out which cities of the world are the leading centers in research of superlubricity. This is what we change in the code:
+
+search_query = 'TS=superlubricity'
+our_org = ''
+
+As of March, 3rd, 2022, the program prints out the following:
+
+This research is concentrated in the following cities:
+
+Beijing: 278
+Lanzhou: 97
+Xian: 42
+Tel Aviv: 33
+Chengdu: 32
+Argonne: 26
+Shenzhen: 25
+Ecully: 24
+Nanjing: 21
+Shanghai: 19
+
+Top 10 collaborating cities shown. All cities are saved to cities.csv file.
+
+
+2. Understand which cities does a specific organization collaborate with
+
+Example: we want to check which cities have been most actively collaborating with Leiden University in 2020. This is what we change in the code:
+
+search_query = 'OG=Leiden University and PY=2020'
+our_org = 'Leiden University'
+
+As of March, 3rd, 2022, the program prints out the following:
+
+Leiden University located in Leiden collaborates with:
+
+Leiden: 2128
+Amsterdam: 1754
+Utrecht: 988
+Rotterdam: 980
+London: 691
+Nijmegen: 673
+Groningen: 637
+Cambridge: 464
+Paris: 432
+The Hague: 424
+
+Top 10 collaborating cities shown. All cities are saved to cities.csv file.
+
+
+3. Figure out in which cities a specific organization is conducting its research
+
+Example: we want to analyze where Pfiser was concentrating its research in 2020
+
+search_query = 'OG=Pfizer and PY=2020'  # Enter the WoS search query here
+our_org = 'Pfizer'
+
+As of March, 3rd, 2022, the .csv file produced by the program will contain the following:
+
+Search Query:	OG=Pfizer and PY=2020
+Results Found:	1606
+Organisation:	Pfizer
+Located in:	
+City		Occurrences
+New York	572
+Groton		466
+Collegeville	289
+Cambridge	259
+San Diego	125
+La Jolla	75
+Andover		73
+Tadworth	55
+Pearl River	53
+San Francisco	50
+...and 72 more cities, as well as a separate list of the cities in which the organizations which collaborate with Pfizer are located.
+
 
 As always, we welcome any feedback to further improve this algorithm.
--- cities_collaborations_analysis/main.py
+++ cities_collaborations_analysis/main.py
@@ -1,140 +1,119 @@
 import requests
 import urllib.parse
-from datetime import date
-from apikey import apikey   # Your API key, it's better not to store it in the program
+from apikey import apikey  # Your API key, it's better not to store it in the program
 
-search_query = 'ts=(energ* and fuel*) and CU=Poland and py=2020'  # Enter the WoS search query here
-our_org = ''  # (OPTIONAL) Enter the organization you'd like to analyze for city collaboration.
+search_query = 'OG=Pfizer and PY=2020'  # Enter the WoS search query here
+our_org = 'Pfizer'  # (OPTIONAL) Enter the organization you'd like to analyze for city collaboration.
 
-headers = {
-    'X-APIKey': apikey  # our API key is passed in the header of the API request
-}
+headers = {'X-APIKey': apikey}  # our API key is passed in the header of the API request
 
 baseurl = "https://api.clarivate.com/api/wos"  # this is the base URL for WoS Expanded API
 
 cities = []  # The cities are to be stored in a list
-# If there is an organizational profile in the search query or "our_org" field, the program will store the list of
+# If there is an existing organizational profile mentioned in the "our_org" field, the program will store the list of
 # cities associated with this org to keep them separate from the collaborating cities list
 our_org_cities = []
 
-# Just a simple loop to make sure data entry is easier: if "our_org" variable is empty and the search is made for "OG=",
-# the program will assume the affiliation in the "OG=" search query is the one being analyzed
-if our_org == '':
-    org_analysis_flag = False
-    if search_query.count('OG=') == 1:
-        org_analysis_flag = True
-        if search_query.count('=') == 1:
-            our_org = search_query[search_query.find('OG=') + 3:]
-        else:
-            our_org_l = search_query[search_query.find('OG=') + 3:]
-            if (
-                    our_org_l.find(' AND ') > -1 or our_org_l.find(' OR ') > -1 or our_org_l.find(' NOT ') > -1 or
-                    our_org_l.find(' and ') > -1 or our_org_l.find(' or ') > -1 or our_org_l.find(' not ') > -1
-            ):
-                existing_booleans = []
-                if our_org_l.find(' AND ') > -1:
-                    existing_booleans.append(our_org_l.find(' AND '))
-                if our_org_l.find(' OR ') > -1:
-                    existing_booleans.append(our_org_l.find(' OR '))
-                if our_org_l.find(' NOT ') > -1:
-                    existing_booleans.append(our_org_l.find(' NOT '))
-                if our_org_l.find(' and ') > -1:
-                    existing_booleans.append(our_org_l.find(' and '))
-                if our_org_l.find(' or ') > -1:
-                    existing_booleans.append(our_org_l.find(' or '))
-                if our_org_l.find(' not ') > -1:
-                    existing_booleans.append(our_org_l.find(' not '))
-                our_org_ending_index = min(existing_booleans)
-                our_org = our_org_l[:our_org_ending_index]
-        if (our_org[0] == '(' and our_org[-1] == ')') or (our_org[0] == '"' and our_org[-1] == '"'):
-            our_org = our_org[1:-1]
-else:
-    org_analysis_flag = True
 
-
-# This function actually gets the cities values from the Web of Science records which we received through the API
+# This function gets the cities values from the Web of Science records which we received through the API
 def analyze_cities(data):
-    for paper in data['Data']['Records']['records']['REC']:  # Analyzing every paper
-        # This variable allows us to make one paper with multiple addresses with the same city counts only once.
-        # Otherwise there would be duplication of city values in the results and we don't want that.
-        cities_cache = []
+    for paper in data['Data']['Records']['records']['REC']:  # This is how we iterate over individual WoS records
         if paper['static_data']['fullrecord_metadata']['addresses']['count'] == 1:  # if there's only 1 address
             for org in (
-                    paper['static_data']['fullrecord_metadata']['addresses']['address_name']['address_spec'][
-                        'organizations']['organization']
-            ):
-                if org['content'] == our_org:  # storing the city belonging to organization being analyzed
+                    paper['static_data']['fullrecord_metadata']['addresses']['address_name']['address_spec']
+                    ['organizations']['organization']
+            ):  # Iterating over individual organizations in the address field
+                if org['pref'] == 'Y' and org['content'] == our_org:  # storing the city belonging to our organization
                     our_org_cities.append(
-                        paper['static_data']['fullrecord_metadata']['addresses']['address_name']['address_spec']
-                        ['city'].title()
+                        paper['static_data']['fullrecord_metadata']['addresses']['address_name']
+                        ['address_spec']['city'].title()
                     )
-            else:  # storing the cities belonging to every other org. Each of them stored as a dictionary in the list
-                for city in cities:
-                    if city['name'] == (
-                            (paper['static_data']['fullrecord_metadata']['addresses']['address_name']['address_spec'][
-                                'city']).title()
-                    ):
-                        city['occurrences'] += 1
-                        break
-                else:
-                    cities.append(
-                        {'name': paper['static_data']['fullrecord_metadata']['addresses']['address_name']
-                         ['address_spec']['city'].title(), 'occurrences': 1}
-                    )
+                    break
+            else:  # storing the cities belonging to every other org. Each of them stored as a list item
+                cities.append(
+                    paper['static_data']['fullrecord_metadata']['addresses']['address_name']
+                    ['address_spec']['city'].title()
+                )
         else:  # Standard case - when there are multiple addresses in the document record
+            # These 2 variables allow us to make multiple addresses with the same city count only once for each paper.
+            cities_cache = []
+            our_cities_cache = []
             try:
+                # Iterating over individual addresses in the Web of Science record
                 for address in paper['static_data']['fullrecord_metadata']['addresses']['address_name']:
-                    for org in address['address_spec']['organizations']['organization']:
-                        if org['pref'] == 'Y' and org['content'] == our_org:
-                            # storing the city belonging to organization being analyzed
-                            our_org_cities.append(address['address_spec']['city'].title())
-                            break
-                    else:
-                        if address['address_spec']['city'].title() not in cities_cache:
-                            cities_cache.append(address['address_spec']['city'].title())
-                            # storing the cities belonging to every other org. Each of them stored as a dictionary in
-                            # the list
-                            for city in cities:
-                                if city['name'] == address['address_spec']['city'].title():
-                                    city['occurrences'] += 1
+                    try:
+                        # Iterating over individual organizations in the address field
+                        for org in address['address_spec']['organizations']['organization']:
+                            if org['pref'] == 'Y' and org['content'] == our_org:
+                                # storing the city belonging to organization being analyzed
+                                if address['address_spec']['city'].title() not in our_cities_cache:
+                                    our_cities_cache.append(address['address_spec']['city'].title())
+                                    our_org_cities.append(address['address_spec']['city'].title())
                                     break
-                            else:
-                                cities.append({'name': address['address_spec']['city'].title(), 'occurrences': 1})
                         else:
-                            pass
+                            if address['address_spec']['city'].title() not in cities_cache:
+                                cities_cache.append(address['address_spec']['city'].title())
+                                # storing the cities belonging to every other org. Each of them stored as a list item
+                                cities.append(address['address_spec']['city'].title())
+                    except KeyError:  # When there's no org data in the address (i.e., only street address)
+                        pass
             except KeyError:  # when there's no address field in the record
                 pass
 
 
 # Initial request to the API is made to figure out the total amount of requests required
 initial_response = requests.get(
-    f'{baseurl}?databaseId=WOS&usrQuery={urllib.parse.quote(search_query)}&count=100&firstRecord=1', headers=headers
+    f'{baseurl}?databaseId=WOS&usrQuery={urllib.parse.quote(search_query)}&count=0&firstRecord=1', headers=headers
 )
-data = initial_response.json()  # First 100 records received
-analyze_cities(data)
+data = initial_response.json()
 total_records = data['QueryResult']['RecordsFound']
-
-# From the first response, extracting the total number of records found and calculating the number of requests required.
-# The program can take up to a few dozen minutes, depending on the number of records being analyzed
-for i in range((total_records - 1) // 100):
+# From the total number of records found in the first API response, calculating the number of requests required.
+# The program can take up to a few dozen minutes to run, depending on the number of records being analyzed
+requests_required = ((total_records - 1) // 100) + 1
+for i in range(requests_required):
     subsequent_response = requests.get(
         f'{baseurl}?databaseId=WOS&usrQuery={urllib.parse.quote(search_query)}&count=100&firstRecord={i}01',
         headers=headers
     )
     data = subsequent_response.json()
     analyze_cities(data)
-    print(f"{((i + 1) * 100) // ((total_records - 1) // 100)}% complete")
+    print(f"{((i + 1) * 100) // (((total_records - 1) // 100) + 1)}% complete")
+
+# Turning lists with stored cities into lists of dictionaries with their names and number of occurrences
+our_org_cities_lod = []
+cities_lod = []
+for city in our_org_cities:
+    if len(our_org_cities_lod) == 0:
+        our_org_cities_lod.append({'name': city, 'occurrences': our_org_cities.count(city)})
+    else:
+        for city_in_lod in our_org_cities_lod:
+            if city_in_lod['name'] == city:
+                break
+        else:
+            our_org_cities_lod.append({'name': city, 'occurrences': our_org_cities.count(city)})
+
+for city in cities:
+    if len(cities_lod) == 0:
+        cities_lod.append({'name': city, 'occurrences': cities.count(city)})
+    else:
+        for city_in_lod in cities_lod:
+            if city_in_lod['name'] == city:
+                break
+        else:
+            cities_lod.append({'name': city, 'occurrences': cities.count(city)})
+
 
 # for the main organization being analyzed, we might get different city values for some of its records. The one
 # appearing most frequently will be the primary city for that org - even if the headquarters might be located elsewhere.
-primary_city = {'name': 'blank', 'occurrences': 0}
-if org_analysis_flag:
-    for city in our_org_cities:
-        if our_org_cities.count(city) > primary_city['occurrences']:
-            primary_city = {'name': city, 'occurrences': our_org_cities.count(city)}
+def occur(our_org_cities_lod):
+    return our_org_cities_lod['occurrences']
+
+
+if our_org != '':
+    our_org_cities_lod.sort(reverse=True, key=occur)
     print(
         f'\nSearch query: {search_query}\nResults found: {total_records}\n'
-        f'\n{our_org} located in {primary_city["name"]} collaborates with:\n'
+        f'\n{our_org} located in {our_org_cities_lod[0]["name"]} collaborates with:\n'
     )
 # For the case where no org is being analyzed (i.e., the records of a specific researcher or a topical search),
 # no base organization is required, and each city that was involved gets into output.
@@ -144,24 +123,27 @@ else:
         f'\nThis research is concentrated in the following cities:\n'
     )
 
-def occur(cities):  # function required as a key to sort the cities on the main list
-    return cities['occurrences']
+
+def occur(cities_lod):  # function required as a key to sort the cities on the main list
+    return cities_lod['occurrences']
 
 
-cities.sort(reverse=True, key=occur)  # sorting the cities by number of papers in which they occurred
-for city in cities:
-    if cities.index(city) < 10:  # printing only the 10 top cities
+cities_lod.sort(reverse=True, key=occur)  # sorting the cities by number of papers in which they occurred
+for city in cities_lod:
+    if cities_lod.index(city) < 10:  # printing only the 10 top cities
         print(f'{city["name"]}: {city["occurrences"]}')
 print(f'\nTop 10 collaborating cities shown. All cities are saved to cities.csv file.')
 
 # All the cities data gets exported in a .csv file. Make sure this file is closed when you run the code again.
-today = date.today()
 with open('cities.csv', 'w') as writing:
-    writing.write(f'Organisation:,{our_org}\n'
-                  f'Located:,{primary_city["name"]}\n'
-                  f'Search Query:,{search_query}\n'
-                  f'Results Found:,{total_records}\n'
-                  f'Search date:,{today.strftime("%m %B %Y")}\n\n'
-                  f'Collaborating Cities,Collaborations\n')
-    for city in cities:
+    writing.write(f'Search Query:,{search_query}\n'
+                  f'Results Found:,{total_records}\n')
+    if our_org != '':
+        writing.write(f'Organisation:,{our_org}\n'
+                      f'Located in:,\n'
+                      f'City,Occurrences\n')
+    for city in our_org_cities_lod:
+        writing.writelines(f'{city["name"]},{city["occurrences"]}\n')
+    writing.write(f'\nCollaborating Cities,Collaborations\n')
+    for city in cities_lod:
         writing.writelines(f'{city["name"]},{city["occurrences"]}\n')
